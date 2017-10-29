085b7b371c83855643144fa1613848525af57352
Ben Klinghoffer <ben.klinghoffer@gmail.com>
2017-10-29 18:56:43 -0400

    initial commit 2

---
+++ desktop/src/com/si/igame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.si.igame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.si.igame.iGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new iGame(), config);
+	}
+}
